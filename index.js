13602b7ec717e90512f24bd0e2f69d9421afdb05
book5 Min chan <kmc130710@daum.net>
2025-08-20 12:43:40 +0900

    erty

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ setInterval(()=>{
   var nv_ads_texts = [Array.from(nv_ads_texts1), ...Array.from(nv_ads_texts2)];
   nv_ads_texts.forEach(e => {
     var ew = e.clientWidth, eh = e.clientHeight;
-    e.innerHTML = `<img src="https://raw.githubusercontent.com/minlab-dev/ads_nono/refs/heads/main/img_ads.png" style="width: ${ew}px !important; height: ${eh-3}px !important;">`
+    e.innerHTML = `<img src="https://github.com/minlab-dev/ads_nono/blob/main/img_ads.png?raw=true" style="width: ${ew}px !important; height: ${eh-3}px !important;">`
     e.onclick = (esd) => {
       esd.stopPropagation()
       esd.preventDefault()
